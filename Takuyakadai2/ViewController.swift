2ba7d9fdacd53ef22a9c6d956631ff8e22273985
takuya119 <takuya.nakahara.119@gmail.com>
2022-09-06 08:24:46 +0900

    課題2要件実装

--- Takuyakadai2/ViewController.swift
+++ Takuyakadai2/ViewController.swift
@@ -21,6 +21,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let inputValueOne = Float(inputOne.text!) ?? 0
+        let inputValueTwo = Float(inputTwo.text!) ?? 0
+        let selectedSegment = calculationSegment.selectedSegmentIndex
+        var result: Float
+        
+        if selectedSegment == 0 {
+            result = inputValueOne + inputValueTwo
+            resultLabel.text = String(format: "%.1f", result)
+        } else if selectedSegment == 1 {
+            result = inputValueOne - inputValueTwo
+            resultLabel.text = String(format: "%.1f", result)
+        } else if selectedSegment == 2 {
+            result = inputValueOne * inputValueTwo
+            resultLabel.text = String(format: "%.1f", result)
+        } else {
+            if inputValueTwo != 0 {
+                result = inputValueOne / inputValueTwo
+                resultLabel.text = String(format: "%.1f", result)
+            } else {
+                resultLabel.text = "割る数には０以外を入力してください"
+            }
+        }
+        
+        
     }
 }
 
