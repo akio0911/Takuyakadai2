8dd93ac635b86831efc47c53b85408a47db9de48
takuya119 <takuya.nakahara.119@gmail.com>
2022-09-06 08:08:18 +0900

    UIパーツとコードの紐付け

--- Takuyakadai2/ViewController.swift
+++ Takuyakadai2/ViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var inputOne: UITextField!
+    @IBOutlet weak var inputTwo: UITextField!
+    @IBOutlet weak var calculationSegment: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    }
 }
 
